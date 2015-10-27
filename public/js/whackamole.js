91a90843377e0f7cbd54e3daf0e800443662c4be
Zach <zach@Zachs-MacBook-Pro.local>
2015-10-27 15:44:44 -0500

    added a buildGame function

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,1 +1,10 @@
-console.log("hello, whackamole!");
+// $(document).ready(function(){
+	var whackamole = {
+		buildGame: function(){
+			for(var i = 1; i < 10; i++){
+				var $gameSquare = $('<div>').addClass('game-square').attr('id','square' + i);
+				$gameSquare.appendTo('#game-area');
+			}
+		}
+	}
+// });
