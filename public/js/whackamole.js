e480da7aca870cbc56e8455dc403c04934070107
Zach <zach@Zachs-MacBook-Pro.local>
2015-10-27 17:27:13 -0500

    buildGame works with any square number

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,10 +1,31 @@
 // $(document).ready(function(){
 	var whackamole = {
-		buildGame: function(){
-			for(var i = 1; i < 10; i++){
+
+		getGameAreaHeight: function(){
+			var height = $('#game-area').css('height');
+			return parseInt(height);
+		},
+
+		getGameAreaWidth: function(){
+			var width = $('#game-area').css('width');
+			return parseInt(width);
+		},
+
+		buildGame: function(numOfSquares){
+			var squareHeight = whackamole.getGameAreaHeight()/Math.sqrt(numOfSquares);
+			var squareWidth = whackamole.getGameAreaWidth()/Math.sqrt(numOfSquares);
+
+			$('#game-area').html('');
+
+			for(var i = 1; i <= numOfSquares; i++){
 				var $gameSquare = $('<div>').addClass('game-square').attr('id','square' + i);
 				$gameSquare.appendTo('#game-area');
 			}
+			$('.game-square').css('height',squareHeight);
+			$('.game-square').css('width',squareWidth);
 		}
+
 	}
+
+	whackamole.buildGame(25);
 // });
